c9defc11ff04bfbed261fe2936b115cac5bcaa6f
SuicideG4M3R <suicideg4m3r@gmail.com>
2026-08-28 08:04:11 +0000

    Day 4

--- 04_Day_Strings/04_exercises.py
+++ 04_Day_Strings/04_exercises.py
@@ -41,20 +41,70 @@ print(company[7:])
 
 # Check if Coding For All string contains a word Coding using the method index, find or other methods.
 
-
+word = 'Coding For All'
+print(word.index('Coding')) 
+print(word.find('Coding'))
+print('Coding' in word)
 
 # Replace the word coding in the string 'Coding For All' to Python.
+
+word = 'Coding For All'
+print(word.replace('Coding', 'Python'))
+
 # Change "Python for Everyone" to "Python for All" using the replace method or other methods.
+
+word = 'Python for Everyone'
+print(word.replace('Everyone', 'All'))
+
 # Split the string 'Coding For All' using space as the separator (split()) .
+
+word = 'Coding For All'
+print(word.split(' '))
+
 # "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+
+word = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(word.split(','))
+
 # What is the character at index 0 in the string Coding For All.
+
+word = 'Coding For All'
+print(word[0])
+
 # What is the last index of the string Coding For All.
+
+word = 'Coding For All'
+print(word[-1])
+
 # What character is at index 10 in "Coding For All" string.
+
+word = 'Coding For All'
+print(word[10])
+
 # Create an acronym or an abbreviation for the name 'Python For Everyone'.
+
+target_word = 'Python For Everyone'
+acronym = ''.join(word[0] for word in target_word.split()).upper()
+print(acronym)
+
 # Create an acronym or an abbreviation for the name 'Coding For All'.
+
+target_word = 'Coding For All'
+acronym = ''.join(word[0] for word in target_word.split()).upper()
+print(acronym)
+
 # Use index to determine the position of the first occurrence of C in Coding For All.
 # Use index to determine the position of the first occurrence of F in Coding For All.
+
+word = 'Coding For All'
+print(word.index('C'))
+print(word.index('F'))
+
 # Use rfind to determine the position of the last occurrence of l in Coding For All People.
+
+word = 'Coding For All People'
+print(word.rfind('l'))
+
 # Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
 # Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
 # Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
